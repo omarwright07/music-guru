9555a2cfeb814c1832dbfa1fc49d1f7049ec5541
bagl0025 <bagl0025@umn.edu>
2022-04-18 11:05:48 -0500

    api functions for genre, url, and lyrics

--- assets/js/tech.js
+++ assets/js/tech.js
@@ -4,6 +4,75 @@ console.log("tech.js ran");
 var placeholder = "";
 var apiURL = "";
 
+// get genre and url
+// need to pass artist name, currently hardcoded
+var getArtistInfo = function() {
+    var lastFMapiBase = "http://ws.audioscrobbler.com/2.0/";
+    var lastFMapiKey = "62f327ad180cacdfe336a5096e041eb9";
+    var artist = "nirvana";   // need to replace this with form input
+    var lastFMapiCall = lastFMapiBase + "?method=artist.getinfo&artist=" + artist + "&api_key=" + lastFMapiKey + "&format=json";
+    
+    fetch(lastFMapiCall).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+            console.log(data.artist.url);
+            document.getElementById("genres-found").append(data.artist.url);
+            for (var i = 0; i < data.artist.tags.tag.length; i++){
+                console.log(data.artist.tags.tag[i].name);
+                document.getElementById("genres-found").append(data.artist.tags.tag[i].name);
+            }
+        });  
+    });
+};  
+
+// ####  Itunes API - Probably not useful  ####
+// itunes api, no auth or key required, have to use jQuery getJSON with callback
+// or you get a text file returned
+// $.getJSON("https://itunes.apple.com/search?term=jack+johnson&entity=musicArtist&callback=?", function (iData) {
+//     console.log(iData);
+// });
+
+// Lyrics
+// https://lyricsovh.docs.apiary.io/
+// The only issue I've found with this is that it can be slow
+
+var getSongLyrics = function() {
+    var lyricBase = "https://api.lyrics.ovh/v1/";
+    var artist = "coldplay";  //get from form input
+    var song = "yellow";  //get from form input
+    var lyricRequest = lyricBase + artist + "/" + song;
+
+    fetch(lyricRequest).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data); 
+            console.log(response.status);       
+            if (response.status != 200){
+                document.getElementById("lyrics").innerHTML=("Song not found");
+            }
+            else {
+                var songLyrics = data.lyrics.split('\n').join('<br />');
+                document.getElementById("lyrics").innerHTML=songLyrics;
+            }
+        })
+    });
+};
+
+// ####### NEED CORRECT MBID FOR THIS TO WORK #######
+// get label 
+// lastFM is returning the wrong MBID
+
+// var getLabel = function(mbid) {
+// musicBrainzApi = "http://musicbrainz.org/ws/2/label/" + "46f0f4cd-8aab-4b33-b698-f459faf64190" + "?inc=aliases&fmt=json";
+// fetch(musicBrainzApi).then(function(response) {
+//     response.json().then(function(data) {
+//         console.log(data);
+//     });
+// });
+// };
+
+getArtistInfo();
+getSongLyrics();  
+
 // ###########################################################
 // ###########################################################
 
